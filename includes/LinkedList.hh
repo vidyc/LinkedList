5b287707ee54023d45f81428b9d76391c6e3e189
vidyc <adrimart00@hotmail.es>
2019-03-03 22:22:18 +0100

    Add files via upload

---
+++ includes/LinkedList.hh
@@ -0,0 +1,163 @@
+#include "Node.hh"
+#include <iostream>
+
+template<class T> class LinkedList {
+	private: 
+	Node<T> *head, *tail;
+	unsigned int size;
+	public:
+	LinkedList() {
+		head = nullptr;
+		tail = nullptr;
+		size = 0;
+	}
+
+	~LinkedList() {
+
+	}
+
+	//metodos basicos
+	void insert_top(T data) {
+		Node<T> *temp = new Node<T>;
+		temp->setData(data);
+		if (head == nullptr) {
+			head = temp;
+			tail = temp;
+		} else {
+			temp->setNext(head);
+			head = temp;	
+		}
+		size++;
+	}
+	void insert_bottom(T data) {
+		Node<T> *temp = new Node<T>;
+		temp->setData(data);
+		if (head == nullptr) {
+			head = temp;
+			tail = temp;
+		} else {
+			tail->setNext(temp);
+			tail = temp;
+		}
+		size++;
+	}
+	void insert(T data, int pos) {
+		if (pos > 0 and pos < size) {
+			Node<T> *pre = new Node<T>, *curr = new Node<T>, *temp = new Node<T>;
+			curr = head;
+			pre = head;
+			for (int i = 0; i < pos; i++) {
+				pre = curr;
+				curr = curr->getNext();
+			}
+			temp->setData(data);
+			temp->setNext(curr);
+			pre->setNext(temp);
+			size++;
+		}
+		else if (pos == 0) insert_top(data);
+		else if (pos == size) insert_bottom(data);
+		else std::cout << "index not valid" << std::endl;
+	}
+	void delete_top() {
+		if (size > 0) {
+			Node<T> *temp = new Node<T>;
+			temp = head;
+			head = head->getNext();
+			delete temp;
+			temp = nullptr;
+			size--;
+		}
+	}
+	void delete_bottom() {
+		if (head == tail) {
+			head = nullptr;
+			tail = nullptr;
+			size = 0;
+		}
+		else {
+			Node<T> *prev = new Node<T>, *curr = new Node<T>;
+			curr = head;
+			while (curr->getNext() != nullptr) {
+				prev = curr;
+				curr = curr->getNext();
+			}
+			tail = prev;
+			prev->setNext(nullptr);
+			delete curr;
+			curr = nullptr;
+			size--;
+		}
+	}
+	void delete_pos(int pos) {
+		if (size > 0 and pos > 0 and pos < size - 1) {
+			Node<T> *prev = new Node <T>, *curr = new Node<T>;
+			curr = head;
+			for (int i = 0; i < pos; i++) {
+				prev = curr;
+				curr = curr->getNext();
+			}
+			prev->setNext(curr->getNext());
+			delete curr;
+			curr = nullptr;
+			size--;
+		}
+		else if (pos == 0) delete_top();
+		else if (pos == size - 1) delete_bottom();
+		else std::cout << "index not valid" << std::endl;
+	}
+	void print() {
+		Node<T> *temp = head;
+		while (temp != nullptr) {
+			temp->print();
+			temp = temp->getNext();
+		}
+		std::cout << std::endl;
+	}
+	int getSize() {return size;}
+
+	T getHead() {
+		if (size > 0) throw std::logic_error("List is empty!");
+		return head->getData();
+	}
+
+	T getTail() {
+		if (size > 0) throw std::logic_error("List is empty!");
+		return tail->getData();
+	}
+
+	T get(int pos) {
+		if (pos >= 0 and pos < size) {
+			Node<T> *temp = new Node<T>;
+			temp = head;
+			for (int i = 0; i < pos; i++) {
+				temp = temp->getNext();
+			}
+			return temp->getData();
+		}
+		else throw std::invalid_argument("Invalid index!");
+	}
+	//metodos sofisticados
+	void swap(Node<T> *n1, Node<T> *n2) {
+		Node<T> *temp = n1;
+		n1 = n2;
+		n2 = n1;
+	}
+
+	void sort() {
+		sort(head);
+	}
+
+	private:
+	void sort(Node<T> *head) {
+		if (head->getNext() != nullptr) {
+			Node<T> *temp = head->getNext();
+			sort(temp);
+			if (temp->getData() < head->getData()) {
+				swap(temp, head);
+				sort(head);
+			}
+		}
+	}
+
+};
